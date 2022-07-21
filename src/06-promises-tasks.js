29855ac62cbb2f1edd21a501daa769a39dbe8a30
svt1108 <svt_1108@inbox.ru>
2022-07-21 18:39:56 +0300

    feat: finish promises

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,13 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+    return new Promise((resolve, reject) => {
+        if (isPositiveAnswer === true) return resolve('Hooray!!! She said "Yes"!');
+        if (isPositiveAnswer === false) return resolve('Oh no, she said "No".');
+        return reject(new Error('Wrong parameter is passed! Ask her again.'));
+    });
 }
-
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +50,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+    return Promise.all(array);
 }
 
 /**
@@ -71,8 +73,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+    return Promise.race(array);
 }
 
 /**
@@ -92,13 +94,23 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+
+function chainPromises(array, action) {
+    return array.reduce(async(sum, item) => {
+        try {
+            return await action(
+                await sum.catch(() => null),
+                await item.catch(() => null)
+            );
+        } catch (error) {
+            return sum;
+        }
+    });
 }
 
 module.exports = {
-  willYouMarryMe,
-  processAllPromises,
-  getFastestPromise,
-  chainPromises,
+    willYouMarryMe,
+    processAllPromises,
+    getFastestPromise,
+    chainPromises,
 };
